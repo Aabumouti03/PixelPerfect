b7305afdf56ef1ad22102c00a3a3238495397c8b
Salma Al Yousif <salma1alyousef@gmail.com>
2025-02-09 00:31:09 +0000

    module overview page

--- rework/urls.py
+++ rework/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from client import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('module-overview/', views.module_overview, name='module_overview'),
 ]
