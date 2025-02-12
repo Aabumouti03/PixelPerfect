9be342a6b8e60d916fa7688c6df19bcd7e44917c
Nouf <Noufabdalm@gmail.com>
2025-02-12 12:11:13 +0000

    Modified User model. Removed Questionarre

--- client/admin.py
+++ client/admin.py
@@ -1,74 +1,18 @@
-from django.contrib import admin
-from .models import Program, Module, Questionnaire, Question, QuestionOption, Option
+# from django.contrib import admin
+# from .models import Program, Module
 
-# Inline for Options (within QuestionOption)
-class OptionInline(admin.TabularInline):
-    model = Option
-    extra = 1  # Number of empty forms to display in the admin interface
-    autocomplete_fields = ['question_option']
+# @admin.register(Program)
+# class ProgramAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'get_modules')  # Show Program ID and related modules
+#     filter_horizontal = ('modules',)  # Enables a ManyToMany selection widget
+#     search_fields = ('modules__title',)  # Enable search by module title
 
-# Inline for QuestionOption (within Question)
-class QuestionOptionInline(admin.TabularInline):
-    model = QuestionOption
-    extra = 1
-    inlines = [OptionInline]
+#     def get_modules(self, obj):
+#         """Display associated modules in the admin panel."""
+#         return ", ".join(module.title for module in obj.modules.all())
+#     get_modules.short_description = 'Modules'
 
-# Admin for Program
-@admin.register(Program)
-class ProgramAdmin(admin.ModelAdmin):
-    list_display = ('id', 'get_modules')
-    filter_horizontal = ('modules',)  # Adds a widget for managing ManyToMany fields
-    search_fields = ('modules__title',)  # Enable search by module title
-
-    def get_modules(self, obj):
-        """Display associated modules in the admin list."""
-        return ", ".join(module.title for module in obj.modules.all())
-    get_modules.short_description = 'Modules'
-
-# Admin for Module
-@admin.register(Module)
-class ModuleAdmin(admin.ModelAdmin):
-    list_display = ('title',)
-    search_fields = ('title',)
-
-# Admin for Questionnaire
-@admin.register(Questionnaire)
-class QuestionnaireAdmin(admin.ModelAdmin):
-    list_display = ('title', 'get_questions')
-    search_fields = ('title', 'questions__question_text')
-
-    def get_questions(self, obj):
-        """Display associated questions in the admin list."""
-        return ", ".join(question.question_text for question in obj.questions.all())
-    get_questions.short_description = 'Questions'
-
-# Admin for Question
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'type', 'has_options')
-    search_fields = ('question_text',)
-    list_filter = ('type',)
-    inlines = [QuestionOptionInline]
-
-    def has_options(self, obj):
-        """Check if the question has options."""
-        return obj.option_set is not None
-    has_options.boolean = True
-    has_options.short_description = 'Has Options'
-
-# Admin for QuestionOption
-@admin.register(QuestionOption)
-class QuestionOptionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'question', 'get_options')
-    search_fields = ('question__question_text',)
-
-    def get_options(self, obj):
-        """Display associated options in the admin list."""
-        return ", ".join(option.option_text for option in obj.options.all())
-    get_options.short_description = 'Options'
-
-# Admin for Option
-@admin.register(Option)
-class OptionAdmin(admin.ModelAdmin):
-    list_display = ('option_text', 'question_option')
-    search_fields = ('option_text', 'question_option__question__question_text')
+# @admin.register(Module)
+# class ModuleAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'title')  # Show Module ID and title
+#     search_fields = ('title',)  # Enable search by module title
--- users/admin.py
+++ users/admin.py
@@ -1,58 +1,67 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User, Admin, EndUser
-from client.models import Program, Module
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# from .models import User, Admin, EndUser
+# from client.models import Program, Module
+
+# # ✅ Check if model is already registered to avoid duplicate registration
+# def is_model_registered(model):
+#     return model in admin.site._registry
+
+# # ✅ Ensure Program & Module are registered before being used in `autocomplete_fields`
+# if not is_model_registered(Program):
+#     @admin.register(Program)
+#     class ProgramAdmin(admin.ModelAdmin):
+#         search_fields = ['title']  # Adjust based on your model fields
+
+# if not is_model_registered(Module):
+#     @admin.register(Module)
+#     class ModuleAdmin(admin.ModelAdmin):
+#         search_fields = ['title']  # Adjust based on your model fields
+
+# # ✅ Custom User Admin
+# if not is_model_registered(User):
+#     @admin.register(User)
+#     class UserAdmin(BaseUserAdmin):
+#         fieldsets = (
+#             (None, {'fields': ('username', 'password')}),
+#             ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+#             ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+#             ('Important dates', {'fields': ('last_login', 'date_joined')}),
+#         )
+
+#         add_fieldsets = (
+#             (None, {
+#                 'classes': ('wide',),
+#                 'fields': ('username', 'email', 'password1', 'password2'),
+#             }),
+#         )
+
+#         list_display = ('username', 'email', 'full_name', 'is_active', 'is_staff')
+#         search_fields = ('username', 'email', 'first_name', 'last_name')
+#         ordering = ('last_name', 'first_name')
+
+#         def full_name(self, obj):
+#             return obj.full_name()
+#         full_name.short_description = 'Full Name'
 
 
-# Check if a model is already registered
-def is_model_registered(model):
-    return model in admin.site._registry
+# # ✅ Admin Profile Management
+# if not is_model_registered(Admin):
+#     @admin.register(Admin)
+#     class AdminProfileAdmin(admin.ModelAdmin):
+#         list_display = ('id', 'user', 'user_email')
+#         search_fields = ('user__username', 'user__email')
+
+#         def user_email(self, obj):
+#             return obj.user.email
+#         user_email.short_description = 'Email'
 
 
-# Custom User admin
-if not is_model_registered(User):
-    @admin.register(User)
-    class UserAdmin(BaseUserAdmin):
-        fieldsets = (
-            (None, {'fields': ('username', 'password')}),
-            ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
-            ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-            ('Important dates', {'fields': ('last_login', 'date_joined')}),
-        )
-
-        add_fieldsets = (
-            (None, {
-                'classes': ('wide',),
-                'fields': ('username', 'email', 'password1', 'password2'),
-            }),
-        )
-
-        list_display = ('username', 'email', 'full_name', 'is_active', 'is_staff')
-        search_fields = ('username', 'email', 'first_name', 'last_name')
-        ordering = ('last_name', 'first_name')
-
-        def full_name(self, obj):
-            return obj.full_name()
-        full_name.short_description = 'Full Name'
-
-
-# Admin profile management
-if not is_model_registered(Admin):
-    @admin.register(Admin)
-    class AdminProfileAdmin(admin.ModelAdmin):
-        list_display = ('id', 'user', 'user_email')
-        search_fields = ('user__username', 'user__email')
-
-        def user_email(self, obj):
-            return obj.user.email
-        user_email.short_description = 'Email'
-
-
-# End user management
-if not is_model_registered(EndUser):
-    @admin.register(EndUser)
-    class EndUserAdmin(admin.ModelAdmin):
-        list_display = ('id', 'user', 'program', 'module')
-        list_filter = ('program', 'module')
-        search_fields = ('user__username', 'user__email', 'program__id', 'module__title')
-        autocomplete_fields = ['user', 'program', 'module']
+# # ✅ EndUser Management
+# if not is_model_registered(EndUser):
+#     @admin.register(EndUser)
+#     class EndUserAdmin(admin.ModelAdmin):
+#         list_display = ('id', 'user', 'program', 'module', 'sector', 'age')
+#         list_filter = ('program', 'module', 'sector', 'gender')
+#         search_fields = ('user__username', 'user__email', 'program__title', 'module__title', 'sector')
+#         autocomplete_fields = ['user', 'program', 'module'] 
