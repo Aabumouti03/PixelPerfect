fb9ce5607f9e80c3583474cb3cd33d8484f954b7
Fatimah Al Yousef <alyousefatimah58@hotmail.com>
2025-02-13 20:28:48 +0000

    import user model

--- client/views.py
+++ client/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from users.models import EndUser
 
 # Create your views here.
 def client_dashboard(request):
     return render(request, 'client_dashboard.html')
 
 def users_management(request):
-    return render(request, 'users_management.html')
+    users = EndUser.objects.all()
+    return render(request, 'users_management.html', {'users': users})
