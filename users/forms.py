6913188587688c733944d7828c0882bf516df3a4
Durrah Al Abduljabbar k21172207 <durrahalabduljabbar@gmail.com>
2025-02-13 11:55:20 +0000

    Put unfinished models as comments to fix later.

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,21 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import EndUser
 
-class SignUpForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=50)
-    last_name = forms.CharField(label='Last Name')
-    email = forms.EmailField(label='Email')
-    gender = forms.CharField(label='Gender')
-    age = forms.CharField(label='Age')
-    sector = forms.CharField(label='Sector')
-    last_time_worked = forms.CharField(label='Last time worked')
-    ethnicity = forms.CharField(label='Ethnicity') #optional
-    phone_number = forms.CharField(label='Phone Number') #optional
-    password = forms.CharField(label='Password', widget=forms.PasswordInput())
-    password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
+class SignUpForm(UserCreationForm):
+    password_confirmation = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput()
+    )
 
+    
+    def clean(self):
+        """Validate password and password confirmation match."""
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password1")
+        password_confirmation = cleaned_data.get("password_confirmation")
+
+        if password and password_confirmation and password != password_confirmation:
+            self.add_error('password_confirmation', "Passwords do not match.")
+
+        return cleaned_data
