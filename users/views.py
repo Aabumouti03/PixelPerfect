7c211998ba2522c3ca1f661b09abb33608154997
Durrah Al Abduljabbar k21172207 <durrahalabduljabbar@gmail.com>
2025-02-16 13:12:04 +0000

    Complete sign up view function.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .forms import SignUpForm
+from .forms import UserSignUpForm, EndUserProfileForm
 
 # Create your views here.
 
@@ -14,5 +14,23 @@ def log_in(request):
 
 #A function for displaying a sign up page
 def sign_up(request):
-    form = SignUpForm()
-    return render(request, 'sign_up.html', {'form': form})
+    if request.method == "POST":
+        user_form = UserSignUpForm(request.POST)
+        profile_form = EndUserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            return redirect('log_in')
+
+    else:
+        user_form = UserSignUpForm()
+        profile_form = EndUserProfileForm()
+
+    return render(request, 'sign_up.html', {'user_form': user_form, 'profile_form': profile_form})
+
+#A function for redirecting the user to the welcome page after logging out
+def log_out(request):
+    pass
