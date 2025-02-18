9b266cd79ab9e4049182ee6d8f7525298ef1e1e4
Nouf <Noufabdalm@gmail.com>
2025-02-18 14:29:09 +0000

    WIP: Checking out

--- users/models.py
+++ users/models.py
@@ -1,52 +1,52 @@
-from django.db import models
-from django.core.validators import RegexValidator
-from django.contrib.auth.models import AbstractUser
-from libgravatar import Gravatar
-from client.models import Program 
+# from django.db import models
+# from django.core.validators import RegexValidator
+# from django.contrib.auth.models import AbstractUser
+# from libgravatar import Gravatar
+# from client.models import Program 
 
-class User(AbstractUser):
-    """Model used for user authentication, and team member related information."""
-    username = models.CharField(
-        max_length=30,
-        unique=True,
-        validators=[RegexValidator(
-            regex=r'^\w{3,}$',
-            message='Username must consist of @ followed by at least three alphanumericals'
-        )]
-    )
-    first_name = models.CharField(max_length=50, blank=False)
-    last_name = models.CharField(max_length=50, blank=False)
-    email = models.EmailField(unique=True, blank=False)
+# class User(AbstractUser):
+#     """Model used for user authentication, and team member related information."""
+#     username = models.CharField(
+#         max_length=30,
+#         unique=True,
+#         validators=[RegexValidator(
+#             regex=r'^\w{3,}$',
+#             message='Username must consist of @ followed by at least three alphanumericals'
+#         )]
+#     )
+#     first_name = models.CharField(max_length=50, blank=False)
+#     last_name = models.CharField(max_length=50, blank=False)
+#     email = models.EmailField(unique=True, blank=False)
 
 
-    class Meta:
-        """Model options."""
+#     class Meta:
+#         """Model options."""
 
-        ordering = ['last_name', 'first_name']
+#         ordering = ['last_name', 'first_name']
 
-    def full_name(self):
-        """Return a string containing the user's full name."""
+#     def full_name(self):
+#         """Return a string containing the user's full name."""
 
-        return f'{self.first_name} {self.last_name}'
+#         return f'{self.first_name} {self.last_name}'
 
-    def gravatar(self, size=120):
-        """Return a URL to the user's gravatar."""
+#     def gravatar(self, size=120):
+#         """Return a URL to the user's gravatar."""
 
-        gravatar_object = Gravatar(self.email)
-        gravatar_url = gravatar_object.get_image(size=size, default='mp')
-        return gravatar_url
+#         gravatar_object = Gravatar(self.email)
+#         gravatar_url = gravatar_object.get_image(size=size, default='mp')
+#         return gravatar_url
 
-    def mini_gravatar(self):
-        """Return a URL to a miniature version of the user's gravatar."""
+#     def mini_gravatar(self):
+#         """Return a URL to a miniature version of the user's gravatar."""
         
-        return self.gravatar(size=60)
+#         return self.gravatar(size=60)
 
 
-class Admin(models.Model):
-    user =  models.OneToOneField(User, on_delete=models.CASCADE, related_name='admin_profile')
+# class Admin(models.Model):
+#     user =  models.OneToOneField(User, on_delete=models.CASCADE, related_name='admin_profile')
 
-class EndUser(models.Model):
-    user =  models.OneToOneField(User, on_delete=models.CASCADE, related_name='User_profile')
-    program = models.OneToOneField(Program, on_delete=models.CASCADE,related_name="User_program" )
-    module = models.ForeignKey('module', on_delete=models.CASCADE, related_name="User_modules")
+# class EndUser(models.Model):
+#     user =  models.OneToOneField(User, on_delete=models.CASCADE, related_name='User_profile')
+#     program = models.OneToOneField(Program, on_delete=models.CASCADE,related_name="User_program" )
+#     module = models.ForeignKey('module', on_delete=models.CASCADE, related_name="User_modules")
     
