8360da10999f1b7e1273b77c1e3c00dd6f7de063
Yara Alkhelaiwi <yaralkhelaiwi@gmail.com>
2025-02-19 12:57:45 +0000

    Change file structure

--- rework/settings.py
+++ rework/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,7 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',  
+    #BASE_DIR / 'static',  
+    os.path.join(BASE_DIR, "client", "static"),
+    os.path.join(BASE_DIR, "users", "static")
 ]
 
 # Default primary key field type
