ae78804ae5d57ef0ef0c2e953e285c2ef36dc218
Raneem Almeshal <163947408+raneemaaa@users.noreply.github.com>
2025-02-20 15:10:50 +0000

    Saving changes before merging

--- users/views.py
+++ users/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
+from client.models import Module, Program
 
 # Create your views here.
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    # Fetch the first program (or a specific program)
+    program = Program.objects.first()  # Adjust this query as needed
+    program_modules = program.modules.all() if program else []
+
+    # Fetch all modules for the modules container
+    modules = Module.objects.all()
+
+     # Add progress data to modules (example: random progress for demonstration)
+    for module in modules:
+        module.progress = 50  # Replace with actual progress logic
+
+    #total_exercises = module.exercises.count()
+    #completed_exercises = module.user_progress.filter(status='completed').count()
+    #module.progress = (completed_exercises / total_exercises) * 100 if total_exercises > 0 else 0
+
+    context = {
+        'user': request.user,
+        'program': program,
+        'program_modules': program_modules,
+        'modules': modules,
+    }
+    return render(request, 'dashboard.html', context)
 
 def modules(request):
     return render(request, 'modules.html')
