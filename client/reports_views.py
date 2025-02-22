4cad7ca6c0dc2af34f54ae30fcca85fb1a13528b
Fatimah Al Yousef <alyousefatimah58@hotmail.com>
2025-02-22 23:10:18 +0000

    add report views file

---
+++ client/reports_views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from client.modules_statistics import * 
+from client.programs_statistics import * 
+import json
+
+def reports(request):
+    # THREE CATEGORIES USERS - MODULES - PROGRAMMS 
+    enrollment_labels, enrollment_data = get_module_enrollment_stats() # 1 for modules 
+    last_work_labels, last_work_data = get_users_last_work_time() #  2 for users
+    program_labels, program_data = get_program_enrollment_stats() # 3 for programs
+
+    return render(request, 'client/reports.html', {
+            'enrollment_labels': json.dumps(enrollment_labels),
+            'enrollment_data': json.dumps(enrollment_data),  
+            'last_work_labels': json.dumps(last_work_labels),
+            'last_work_data': json.dumps(last_work_data),
+            'program_labels': json.dumps(program_labels),
+            'program_data': json.dumps(program_data),
+        })
+
+def modules_statistics(request):
+    """Main view function to fetch and pass module statistics."""
+    enrollment_labels, enrollment_data = get_module_enrollment_stats()
+    completion_labels, completed_data, in_progress_data = get_module_completion_stats()
+    avg_completion_labels, avg_completion_data = get_average_completion_percentage()
+    modules_count = get_modules_count()
+
+    return render(request, 'client/modules_statistics.html', {
+        'modules_count': modules_count,
+        'enrollment_labels': json.dumps(enrollment_labels),
+        'enrollment_data': json.dumps(enrollment_data),
+        'completion_labels': json.dumps(completion_labels),
+        'completed_data': json.dumps(completed_data),
+        'in_progress_data': json.dumps(in_progress_data),
+        'completion_time_labels': json.dumps(avg_completion_labels),
+        'completion_time_data': json.dumps(avg_completion_data),  
+    })
+
+def programs_statistics(request):
+    """Main view function to fetch and pass program statistics."""
+    
+    program_labels, program_data = get_program_enrollment_stats()
+    completion_labels, completed_data, in_progress_data = get_program_completion_stats()
+    avg_completion_labels, avg_completion_data = get_average_program_completion_percentage()
+    programs_count = get_programs_count()
+
+    return render(request, 'client/programs_statistics.html', {
+        'program_labels': json.dumps(program_labels),
+        'program_data': json.dumps(program_data),
+        'completion_labels': json.dumps(completion_labels),
+        'completed_data': json.dumps(completed_data),
+        'in_progress_data': json.dumps(in_progress_data),
+        'completion_time_labels': json.dumps(avg_completion_labels),
+        'completion_time_data': json.dumps(avg_completion_data),  
+        'programs_count': programs_count
+    })
