865b32594595fa6c76ff93ab040c5de87371550e
Salma Al Yousif <salma1alyousef@gmail.com>
2025-03-07 22:30:04 +0000

    seperate functions from views

--- users/views.py
+++ users/views.py
@@ -16,6 +16,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.db.models import Avg
+from .helpers import calculate_progress  
+
 
 
 
@@ -256,30 +258,6 @@ def mark_done(request):
     return JsonResponse({"success": False})
 
 
-def calculate_progress(end_user, module):
-    # Get all exercises in the module
-    exercises = []
-    for section in module.sections.all():
-        if section.exercises.exists():
-            exercises.extend(section.exercises.all())
-
-    # Count completed exercises
-    completed_exercises = [exercise for exercise in exercises if exercise.status == 'completed']
-
-    # Count completed resources
-    completed_resources = [resource for resource in module.additional_resources.all() if resource.status == 'completed']
-
-    # Calculate total and completed items
-    total_items = len(exercises) + module.additional_resources.count()
-    completed_items = len(completed_exercises) + len(completed_resources)
-
-    # Calculate progress percentage
-    if total_items > 0:
-        progress = (completed_items / total_items) * 100
-    else:
-        progress = 0
-
-    return progress
 
 
 
